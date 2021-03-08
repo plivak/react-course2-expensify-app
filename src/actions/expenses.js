90276bb05753885c7b99e566577dea210facfc46
Paul Livak <plivak@gmail.com>
2021-03-07 19:54:24 -0500

    This one has getfirebase expenses

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -1,8 +1,6 @@
 import uuid from 'uuid';
 import database from '../firebase/firebase';
 
-
-
 // ADD_EXPENSE
 export const addExpense = (expense) => ({
     type: 'ADD_EXPENSE',
@@ -19,8 +17,8 @@ export const addExpense = (expense) => ({
       } = expenseData;
 
       const expense = { description, note, amount, createdAt };
-      
-      database.ref('expenses').push(expense).then((ref) => {
+
+      return database.ref('expenses').push(expense).then((ref) => {
         dispatch(addExpense({
           id: ref.key,
           ...expense
@@ -41,4 +39,30 @@ export const addExpense = (expense) => ({
     id,
     updates
   });
+
+  //SET_EXPENSES
+  export const setExpenses = (expenses) => ({
+    type: 'SET_EXPENSES', 
+    expenses
+  });
+
+
+    // fetches data asynchronously
+  export const startSetExpenses = () => {
+    return (dispatch) => {
+      return database.ref('expenses').once('value').then((snapshot) => {
+        const expenses = [];
+
+        snapshot.forEach((childSnapshot) => {
+          expenses.push({
+              id: childSnapshot.key,
+              ...childSnapshot.val()
+          });
+        });
+        dispatch(setExpenses(expenses));
+
+      });
+    };
+  };
+
   
